380afacefa76fef3770ef0b6cfd31c469d5970e9
Luan Fonseca <luanfonsecap@icloud.com>
2022-05-07 17:28:25 -0300

    adiciona tela de carregamento

--- App.tsx
+++ App.tsx
@@ -1,9 +1,26 @@
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import {
+  useFonts,
+  Montserrat_400Regular,
+  Montserrat_500Medium,
+  Montserrat_700Bold,
+} from "@expo-google-fonts/montserrat";
+import AppLoading from "expo-app-loading";
 
 import { Routes } from "./src/routes";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Montserrat_400Regular,
+    Montserrat_500Medium,
+    Montserrat_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <SafeAreaProvider>
       <StatusBar style="light" backgroundColor="#312e38" />
